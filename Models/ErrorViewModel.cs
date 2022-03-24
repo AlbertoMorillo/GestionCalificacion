bd44ae151408fdcbf3c451e503ddeeb21d55c73a
Alberto Morillo <amorillo@activeit.cl>
2022-03-23 23:23:54 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GestionCalificacion.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
